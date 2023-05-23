76d3bbd8a7d31f061885f0fc29e3c1981269f40c
kevdog <kevdog@github.com>
2023-05-23 15:28:07 -0400

    Added manifiest files and cobra initialization

--- main.go
+++ main.go
@@ -1,26 +1,22 @@
+/*
+Copyright © 2023 Kevin Stevens kevdog@github.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package main
 
-import (
-	"fmt"
-
-	"github.com/cli/go-gh/v2/pkg/api"
-)
+import "github.com/KevDog/gh-ghes/cmd"
 
 func main() {
-	fmt.Println("hi world, this is the gh-ghes extension!")
-	client, err := api.DefaultRESTClient()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	response := struct {Login string}{}
-	err = client.Get("user", &response)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("running as %s\n", response.Login)
+	cmd.Execute()
 }
-
-// For more examples of using go-gh, see:
-// https://github.com/cli/go-gh/blob/trunk/example_gh_test.go
